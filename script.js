e4de566d900a9e09be88c8e13199d7a6f70ee258
Misza-prug <archuk0713@gmail.com>
2025-05-25 15:11:59 +0200

    9

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('zyczeniaBtn').addEventListener('click', () => {
-  alert('Wszystkiego najlepszego z okazji Dnia Matki! ❤️🌷');
+  alert('Niech moja wdzięczność i miłość będą dla Ciebie najpiękniejszym prezentem. Mamusiu, w dniu Twojego święta życzę Ci mnóstwo radości, bardzo ciebie lubie zdrowia i spełnienia twoich najskrytszych marzeń bo taka osoba jak Ty zasługuje na same najlepsze rzeczy! Wszystkiego najlepszego z okazji Dnia Matki! ❤️🌷');
 });
