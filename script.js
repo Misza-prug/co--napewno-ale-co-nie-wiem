eb026b6a96d66ecab7237dc3bd598dc586cdaf74
Misza-prug <archuk0713@gmail.com>
2025-05-25 15:07:13 +0200

    8

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-
-function createFlower() {
-      const flower = document.createElement('div');
-      flower.classList.add('flower');
-      flower.style.left = Math.random() * window.innerWidth + 'px';
-      const duration = Math.random() * 4 + 4; // Losowy czas animacji od 4 do 8 sekund
-      flower.style.animation = `fall ${duration}s linear`;
-      document.body.appendChild(flower);
-
-      setTimeout(() => {
-        flower.remove();
-      }, duration * 1000);
-    }
-
-    setInterval(createFlower, 300); // Tworzenie kwiatów co 300ms dla większej gęstości
+document.getElementById('zyczeniaBtn').addEventListener('click', () => {
+  alert('Wszystkiego najlepszego z okazji Dnia Matki! ❤️🌷');
+});
